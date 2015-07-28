1f240dbb549366fea73286b5e04b5e4e93984237
FuriousAngelok <solnushkina@yandex.ru>
2015-07-28 14:51:05 +0300

    Update routes.rb
    An error occurred while loading the routes definition of redmine_time_tracker plugin (/opt/redmine/plugins/redmine_time_tracker/config/routes.rb): You should not use the `match` method in your router without specifying an HTTP method.
    If you want to expose your action to both GET and POST, add `via: [:get, :post]` option.
    If you want to expose your action to GET, use `get` in the router:
      Instead of: match "controller#action"
      Do: get "controller#action".

--- config/routes.rb
+++ config/routes.rb
@@ -8,42 +8,42 @@ resources :projects do
   resources :report_queries, :only => [:new, :create]
 end
 
-match '/time_bookings/actions', :to => 'time_bookings#actions', :via => [:get, :post, :put]
-match '/time_bookings/show_edit', :to => 'time_bookings#show_edit', :via => [:get, :post]
-match '/time_bookings/delete', :to => 'time_bookings#delete', :via => [:get, :post]
-match '/time_bookings/get_list_entry', :to => 'time_bookings#get_list_entry', :via => [:get, :post]
+get '/time_bookings/actions', :to => 'time_bookings#actions', :via => [:get, :post, :put]
+get '/time_bookings/show_edit', :to => 'time_bookings#show_edit', :via => [:get, :post]
+get '/time_bookings/delete', :to => 'time_bookings#delete', :via => [:get, :post]
+get '/time_bookings/get_list_entry', :to => 'time_bookings#get_list_entry', :via => [:get, :post]
 
-match '/time_logs/actions', :to => 'time_logs#actions', :via => [:get, :post, :put]
-match '/time_logs/delete', :to => 'time_logs#delete', :via => [:get, :post]
-match '/time_logs/show_booking', :to => 'time_logs#show_booking', :via => [:get, :post]
-match '/time_logs/show_edit', :to => 'time_logs#show_edit', :via => [:get, :post]
-match '/time_logs/get_list_entry', :to => 'time_logs#get_list_entry', :via => [:get, :post]
+get '/time_logs/actions', :to => 'time_logs#actions', :via => [:get, :post, :put]
+get '/time_logs/delete', :to => 'time_logs#delete', :via => [:get, :post]
+get '/time_logs/show_booking', :to => 'time_logs#show_booking', :via => [:get, :post]
+get '/time_logs/show_edit', :to => 'time_logs#show_edit', :via => [:get, :post]
+get '/time_logs/get_list_entry', :to => 'time_logs#get_list_entry', :via => [:get, :post]
 
-match '/tt_overview', :to => 'tt_overview#index', :via => [:get, :post]
+get '/tt_overview', :to => 'tt_overview#index', :via => [:get, :post]
 
-match '/time_trackers/stop', :to => 'time_trackers#stop', :via => [:get, :put, :post]
-match '/time_trackers/start', :to => 'time_trackers#start', :via => [:post, :get]
-match '/time_trackers/update', :to => 'time_trackers#update', :via => [:get, :put]
-match '/time_trackers/delete', :to => 'time_trackers#delete', :via => [:get, :post]
+get '/time_trackers/stop', :to => 'time_trackers#stop', :via => [:get, :put, :post]
+get '/time_trackers/start', :to => 'time_trackers#start', :via => [:post, :get]
+get '/time_trackers/update', :to => 'time_trackers#update', :via => [:get, :put]
+get '/time_trackers/delete', :to => 'time_trackers#delete', :via => [:get, :post]
 
-match '/tt_bookings_list', :to => 'tt_bookings_list#index', :via => [:get, :post]
-match '/tt_info', :to => 'tt_info#index', :via => [:get, :post]
-match '/tt_logs_list', :to => 'tt_logs_list#index', :via => [:get, :post]
-match '/tt_print_report', :to => 'tt_reporting#print_report', :via => [:get]
-match '/tt_reporting', :to => 'tt_reporting#index', :via => [:get, :post]
+get '/tt_bookings_list', :to => 'tt_bookings_list#index', :via => [:get, :post]
+get '/tt_info', :to => 'tt_info#index', :via => [:get, :post]
+get '/tt_logs_list', :to => 'tt_logs_list#index', :via => [:get, :post]
+get '/tt_print_report', :to => 'tt_reporting#print_report', :via => [:get]
+get '/tt_reporting', :to => 'tt_reporting#index', :via => [:get, :post]
 
 #helpers
-match '/tt_completer/get_issue', :to => 'tt_completer#get_issue', :via => [:get]
-match '/tt_completer/get_issue_id', :to => 'tt_completer#get_issue_id', :via => [:get]
-match '/tt_completer/get_issue_subject', :to => 'tt_completer#get_issue_subject', :via => [:get]
-match '/tt_completer/get_activity', :to => 'tt_completer#get_activity', :via => [:get]
+get '/tt_completer/get_issue', :to => 'tt_completer#get_issue', :via => [:get]
+get '/tt_completer/get_issue_id', :to => 'tt_completer#get_issue_id', :via => [:get]
+get '/tt_completer/get_issue_subject', :to => 'tt_completer#get_issue_subject', :via => [:get]
+get '/tt_completer/get_activity', :to => 'tt_completer#get_activity', :via => [:get]
 
-match '/tt_date_shifter/get_prev_time_span', :to => 'tt_date_shifter#get_prev_time_span', :via => [:get]
-match '/tt_date_shifter/get_next_time_span', :to => 'tt_date_shifter#get_next_time_span', :via => [:get]
+get '/tt_date_shifter/get_prev_time_span', :to => 'tt_date_shifter#get_prev_time_span', :via => [:get]
+get '/tt_date_shifter/get_next_time_span', :to => 'tt_date_shifter#get_next_time_span', :via => [:get]
 
-match '/tt_menu_switcher/index', :to => 'tt_menu_switcher#index', :via => [:get]
+get '/tt_menu_switcher/index', :to => 'tt_menu_switcher#index', :via => [:get]
 
 # context menus
-match '/tt_overview/context_menu', :to => 'context_menus#tt_overview', :as => 'tt_overview_context_menu'
-match '/tt_bookings_list/context_menu', :to => 'context_menus#tt_bookings_list', :as => 'tt_bookings_list_context_menu'
-match '/tt_logs_list/context_menu', :to => 'context_menus#tt_logs_list', :as => 'tt_logs_list_context_menu'
+get '/tt_overview/context_menu', :to => 'context_menus#tt_overview', :as => 'tt_overview_context_menu'
+get '/tt_bookings_list/context_menu', :to => 'context_menus#tt_bookings_list', :as => 'tt_bookings_list_context_menu'
+get '/tt_logs_list/context_menu', :to => 'context_menus#tt_logs_list', :as => 'tt_logs_list_context_menu'
